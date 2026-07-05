bbe2c72a34ce9702ced826b095a1608a3306b92f
svtkavi25-sys <svtkavi25@gmail.com>
2026-07-05 16:29:00 +0530

    Solved LeetCode 217 Contains Duplicate

---
+++ 217_contains_duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        self.nums=nums
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    count+=1
+        if count==0:
+            return False
+        else:
+            return True
