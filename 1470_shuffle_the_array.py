60a79075f71adf185fd66c071ca509ee46556d90
svtkavi25-sys <svtkavi25@gmail.com>
2026-07-09 17:19:10 +0530

    Solved LeetCode 1470 Shuffle the Array

---
+++ 1470_shuffle_the_array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        arr1=[]
+        arr2=[]
+        final=[]
+        for i in range(0,n):
+            arr1.append(nums[i])
+        for j in range(n,len(nums)):
+            arr2.append(nums[j])
+        for i in range(n):
+            final.append(arr1[i])
+            final.append(arr2[i])
+        return final
+
+
+        
