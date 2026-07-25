de169f53302cc36913cacf55db0cb108d890cea3
svtkavi25-sys <svtkavi25@gmail.com>
2026-07-25 22:25:18 +0530

    Solved LeetCode 20 Valid Paranthesis

---
+++ 20_Valid_paranthesis.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        for char in s:
+
+            if char == '(' or char == '[' or char == '{':
+                stack.append(char)
+
+            else:
+                if len(stack) == 0:
+                    return False
+
+                top = stack.pop()
+
+                if char == ')' and top != '(':
+                    return False
+
+                if char == ']' and top != '[':
+                    return False
+
+                if char == '}' and top != '{':
+                    return False
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+ 
