28246c7dc58c7f15dfdccbcf6fdb26b39d32af27
svtkavi25-sys <svtkavi25@gmail.com>
2026-07-28 20:41:38 +0530

    Solved LeetCode 2562 Sum Multiple

---
+++ 2562_Sumn_Multiples.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        sum=0
+        for i in range(1,n+1):
+            if(i%3==0 or i%5==0 or i%7==0):
+                sum+=i
+        return sum
+
+        
