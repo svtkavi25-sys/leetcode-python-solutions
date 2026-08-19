bd9a8601eebc06887ce40513aa2fb697d415ed1a
S.V.Kavi <svtkavi25@gmail.com>
2026-08-19 19:54:58 +0530

    Solved leetcode problem 1009. Complement of Base 10 Integer

---
+++ 1009_Complement_of_Base_10_Integer.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def bitwiseComplement(self, n):
+        n1=bin(n)[2:]
+        c1=""
+        for i in range(len(str(n1))):
+            if n1[i]=="0":
+                c1+="1"
+            else:
+                c1+="0"
+        return int(c1,2) 
+        
