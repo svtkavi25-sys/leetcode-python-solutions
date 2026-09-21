bdfae267f0ca887b27e0cc09cddf8f75671cd8a1
S.V.Kavi <svtkavi25@gmail.com>
2026-09-21 20:15:42 +0530

    Solved leetcode problem 1967 Number of Strings That Appear as Substrings in Word

---
+++ 1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numOfStrings(self, patterns, word):
+        count=0
+        for i in range(len(patterns)):
+            if patterns[i] in word:
+                count+=1
+        return count
+        
